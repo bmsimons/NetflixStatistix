4cc7d7fbe2971962abccb1a5d72a05219039e2e2
Floris881 <florisvanwijk1998@gmail.com>
2018-10-12 14:00:01 +0200

    added domain classes to the domain package and changed the way the data layer is setup, split into multiple files and not in 1 file

---
+++ nl.avans.netflixstatistix/src/data/connection/DBConnection.java
@@ -0,0 +1,10 @@
+package data.connection;
+
+public class DBConnection {
+    private static String url = "";
+    private static String drivername = "";
+    private static String username = "";
+    private static String password = "";
+
+    private static
+}
